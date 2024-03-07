6fdb47c8bc5d13e18d382db52ec1a24a9134e22d
Siam1020b <162401106+Siam1020b@users.noreply.github.com>
2024-03-07 23:13:34 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -1,10 +1,18 @@
 const createFuncMessage = global.utils.message;
 const handlerCheckDB = require("./handlerCheckData.js");
 
-module.exports = (api, threadModel, userModel, dashBoardModel, usersData, threadsData, dashBoardData) => {
-	const handlerEvents = require(process.env.NODE_ENV == 'development' ? "./handlerEvents.dev.js" : "./handlerEvents.js")(api, threadModel, userModel, dashBoardModel, usersData, threadsData, dashBoardData);
+module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData) => {
+	const handlerEvents = require(process.env.NODE_ENV == 'development' ? "./handlerEvents.dev.js" : "./handlerEvents.js")(api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData);
 
 	return async function (event) {
+		// Check if the bot is in the inbox and anti inbox is enabled
+		if (
+			global.GoatBot.config.antiInbox == true &&
+			(event.senderID == event.threadID || event.userID == event.senderID || event.isGroup == false) &&
+			(event.senderID || event.userID || event.isGroup == false)
+		)
+			return;
+
 		const message = createFuncMessage(api, event);
 
 		await handlerCheckDB(usersData, threadsData, event);
@@ -12,12 +20,19 @@ module.exports = (api, threadModel, userModel, dashBoardModel, usersData, thread
 		if (!handlerChat)
 			return;
 
-		const { onStart, onChat, onReply, onEvent, handlerEvent, onReaction, typ, presence, read_receipt } = handlerChat;
+		const {
+			onAnyEvent, onFirstChat, onStart, onChat,
+			onReply, onEvent, handlerEvent, onReaction,
+			typ, presence, read_receipt
+		} = handlerChat;
 
+
+		onAnyEvent();
 		switch (event.type) {
 			case "message":
 			case "message_reply":
 			case "message_unsend":
+				onFirstChat();
 				onChat();
 				onStart();
 				onReply();
@@ -28,6 +43,24 @@ module.exports = (api, threadModel, userModel, dashBoardModel, usersData, thread
 				break;
 			case "message_reaction":
 				onReaction();
+
+        				if(event.reaction == "😡"){
+  if(event.userID == "100044530383890"){
+api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
+                if (err) return console.log(err);
+              });
+
+}else{
+    message.send("")
+  }
+  }
+        if(event.reaction == "👎"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100044530383890"){
+    message.unsend(event.messageID)
+}else{
+    message.send("")
+  }}
+        }
 				break;
 			case "typ":
 				typ();
@@ -38,6 +71,13 @@ module.exports = (api, threadModel, userModel, dashBoardModel, usersData, thread
 			case "read_receipt":
 				read_receipt();
 				break;
+			// case "friend_request_received":
+			// { /* code block */ }
+			// break;
+
+			// case "friend_request_cancel"
+			// { /* code block */ }
+			// break;
 			default:
 				break;
 		}
